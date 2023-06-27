d78690170a63d309124d86ccc5bd59bb4e9b93d7
Isabella <isabellaotoo25@gmail.com>
2023-06-27 17:56:49 +0000

    Decorate a class

--- person.rb
+++ person.rb
@@ -1,13 +1,18 @@
+require_relative 'nameable'
+
 # Represents a person with attributes such as name, age, and permission.
-class Person
+class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = generate_id
+    super()
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   def can_use_services?
@@ -18,9 +23,5 @@ class Person
 
   def of_age?
     age >= 18
-  end
-
-  def generate_id
-    rand(1..1000)
   end
 end
