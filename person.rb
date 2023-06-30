72ebd1ccd7170c435135623183b96a368d9fe99a
Isabella <isabellaotoo25@gmail.com>
2023-06-30 16:52:21 +0000

    fix linters

--- person.rb
+++ person.rb
@@ -17,24 +17,3 @@ class Person
     self.class.all << self
   end
 end
-
-
-# class Person
-#   attr_accessor :name, :age
-#   attr_reader :rentals
-
-#   @all = []
-
-#   class << self
-#     attr_reader :all
-#   end
-
-#   def initialize(age, name: 'Unknown', parent_permission: true)
-#     @name = name
-#     @age = age
-#     @parent_permission = parent_permission
-#     @rentals = []
-
-#     self.class.all << self
-#   end
-# end
